eed328ce1334d0390d03606298a60511fb84cff7
yyl <yuanyu.lin@gmail.com>
2018-12-17 17:12:52 +0800

    MAJOR_NUM=1 MINOR_NUM=6 AUX_NUM=0 update package

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -82,6 +82,19 @@ def MAIN_INSTALL(args):
     iopc.installBin(args["pkg_name"], ops.path_join(tmp_include_dir, "."), dst_include_dir) 
     return False
 
+def MAIN_SDKENV(args):
+    set_global(args)
+
+    cflags = ""
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/' + args["pkg_name"])
+    iopc.add_includes(cflags)
+
+    libs = ""
+    libs += " -lffi"
+    iopc.add_libs(libs)
+
+    return False
+
 def MAIN_CLEAN_BUILD(args):
     set_global(args)
     return False
